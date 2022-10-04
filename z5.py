c2e1591d64f2361f13b8fa12782728bcf8fe9949
Timur Minikhairov / Тимур Минихаиров <32126793+Baker224@users.noreply.github.com>
2022-10-04 19:47:27 +0500

    Add files via upload

---
+++ z5.py
@@ -0,0 +1,53 @@
+"""
+Задание 5. На закрепление навыков работы со стеком
+Реализуйте собственный класс-структуру "стопка тарелок".
+Мы можем складывать тарелки в стопку и при превышении некоторого значения
+нужно начать складывать тарелки в новую стопку.
+Структура должна предусматривать наличие нескольких стопок.
+Создание новой стопки происходит при достижении предыдущим
+стеком порогового значения.
+После реализации структуры, проверьте ее работу на различных сценариях.
+Примечание: ПРОШУ ВАС ВНИМАТЕЛЬНО ЧИТАТЬ ЗАДАНИЕ!
+--реализуйте по аналогии с примером, рассмотренным на уроке
+--создание нового стопки можно реализовать добавлением нового пустого массива
+в массив стопок (lst = [[], [], [], [],....]).
+"""
+
+
+class stack_of_plates:
+    def __init__(self):
+        self.stack = []
+        self.max = None
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        removed = self.stack.pop()
+        if len(self.stack) == 0:
+            self.max = None
+        elif removed == self.max:
+            self.max = self.stack[0]
+            for value in self.stack:
+                if value > self.max:
+                    self.max = value
+        return removed
+
+    def push(self, item):
+        self.stack.append(item)
+        if len(self.stack) == 1 or item > self.max:
+            self.max = item
+
+    def get_max(self):
+        return self.max
+
+
+plates = stack_of_plates()
+plates.push(1)
+plates.push(2)
+print(plates.max)
+plates.push(3)
+print(plates.max)
+plates.push(6)
+print(plates.max)
+print(plates.pop())
+print(plates.max)
